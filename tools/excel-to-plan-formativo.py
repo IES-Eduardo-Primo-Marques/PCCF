33a8127f5d1d787b2f7cf41106f1e2e133b8af8c
David <david@martinezpenya.es>
2025-12-09 16:55:01 +0100

    Refactor: mejorar la legibilidad del procesamiento de celdas en el script de generación de planes formativos

--- tools/excel-to-plan-formativo.py
+++ tools/excel-to-plan-formativo.py
@@ -59,28 +59,28 @@ except KeyError as ke:
 
 print(" * [ PCCF ] : Generando plan de Formacion en Empresa ")
 for fila in range(fila_inicio, ws.max_row + 1):
-        celda_actual = ws[f"{columna_a_buscar}{fila}"]
-        # Comprobamos que el valor de la celda no es la suma simple
-        # esto no es lo mejor, pero es que no se me ocurre otra cosa
-        procesar_forzado=False
-        if celda_actual.data_type == 'f' and "SUM" not in str(celda_actual.value):
-            print("  [ Plan Formativo ] : Celda con formula expresada voluntariamente ")
-            procesar_forzado=True
-            
-        if celda_actual.value is not None and celda_actual.data_type != 'f' or procesar_forzado:
-            # Obtener la columna anterior (ej: 'B' si columna_a_buscar es 'C')
-            columna_anterior = chr(ord(columna_a_buscar) - 7)
-            celda_anterior = ws[f"{columna_anterior}{fila}"]
+    celda_actual = ws[f"{columna_a_buscar}{fila}"]
+    # Comprobamos que el valor de la celda no es la suma simple
+    # esto no es lo mejor, pero es que no se me ocurre otra cosa
+    procesar_forzado=False
+    if celda_actual.data_type == 'f' and "SUM" not in str(celda_actual.value):
+        print("  [ Plan Formativo ] : Celda con formula expresada voluntariamente ")
+        procesar_forzado=True
+        
+    if celda_actual.value is not None and celda_actual.data_type != 'f' or procesar_forzado:
+        # Obtener la columna anterior (ej: 'B' si columna_a_buscar es 'C')
+        columna_anterior = chr(ord(columna_a_buscar) - 7)
+        celda_anterior = ws[f"{columna_anterior}{fila}"]
 
-            # Verificar si la celda anterior está fusionada
-            for merged_range in ws.merged_cells.ranges:
-                if celda_anterior.coordinate in merged_range:
-                    # Obtener la celda superior izquierda del rango fusionado
-                    celda_fusionada = ws[merged_range.start_cell.coordinate]
-                    strings.add(str(celda_fusionada.value))
-            strings.add(str(celda_anterior.value))
-        else:
-            pass
+        # Verificar si la celda anterior está fusionada
+        for merged_range in ws.merged_cells.ranges:
+            if celda_anterior.coordinate in merged_range:
+                # Obtener la celda superior izquierda del rango fusionado
+                celda_fusionada = ws[merged_range.start_cell.coordinate]
+                strings.add(str(celda_fusionada.value))
+        strings.add(str(celda_anterior.value))
+    else:
+        pass
 
 ras_ordenados=sorted(strings)
 generar_tabla_markdown(ras_ordenados,hoja_orig)
